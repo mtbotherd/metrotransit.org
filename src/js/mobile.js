f9df8a8fd6624fc27fd77e4505f57d005b322fb6
mtbotherd <mtbotherd@users.noreply.github.com>
2017-09-05 13:48:53 -0500

    Source files.

---
+++ src/js/mobile.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+
+	/* Rider's Almanac redesign */
+	// Remove style attribute from hero images.
+	$('.ra-redesign .blogtext .blogger img[style]').removeAttr('style');
+});
