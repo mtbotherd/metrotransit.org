6f7e08d260f38a911bde3716b78c27116d78a377
mtbotherd <mtbotherd@users.noreply.github.com>
2018-02-16 15:47:50 -0600

    Removed police manual and supporting files.  Made it its own repo.

--- src/js/fed.js
+++ src/js/fed.js
@@ -52,25 +52,18 @@ $(document).ready(function() {
     // Article details
     //$('#ctl00_mainContent_BlogView1_pnlInnerBody #ctl00_mainContent_BlogView1_bsocial').remove();
 
-    // Inject panel body content into corresponding panel
-    $('.panel-body-1').appendTo('#detour1 .panel-body');
-    $('.panel-body-2').appendTo('#detour2 .panel-body');
-    $('.panel-body-3').appendTo('#detour3 .panel-body');
-    $('.panel-body-4').appendTo('#detour4 .panel-body');
-    $('.panel-body-5').appendTo('#detour5 .panel-body');
-    $('.panel-body-6').appendTo('#detour6 .panel-body');
-    $('.panel-body-7').appendTo('#detour7 .panel-body');
-    $('.panel-body-8').appendTo('#detour8 .panel-body');
-    $('.panel-body-9').appendTo('#detour9 .panel-body');
-    $('.panel-body-10').appendTo('#detour10 .panel-body');
-    $('.panel-body-11').appendTo('#detour11 .panel-body');
-    $('.panel-body-12').appendTo('#detour12 .panel-body');
-    $('.panel-body-13').appendTo('#detour13 .panel-body');
-    $('.panel-body-14').appendTo('#detour14 .panel-body');
-    $('.panel-body-15').appendTo('#detour15 .panel-body');
-    $('.panel-body-16').appendTo('#detour16 .panel-body');
-    $('.panel-body-17').appendTo('#detour17 .panel-body');
-    $('.panel-body-18').appendTo('#detour18 .panel-body');
-    $('.panel-body-19').appendTo('#detour19 .panel-body');
-    $('.panel-body-20').appendTo('#detour20 .panel-body');
+    // Ridematch expand/collapse panels
+    $('.panel-collapse').each(function() {
+        $(this).click(function(event) {
+            if (this == event.target) {
+                if ($(this).is('.plus-icon')) {
+                    $(this).removeClass('plus-icon');
+                    $(this).addClass('minus-icon');
+                } else {
+                    $(this).removeClass('minus-icon');
+                    $(this).addClass('plus-icon');
+                }
+            }
+        });
+    });
 });
