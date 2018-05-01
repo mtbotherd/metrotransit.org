4e3c437f0b464017a7c8d55cf79db7024ae48568
mtbotherd <mtbotherd@users.noreply.github.com>
2018-05-01 16:29:43 -0500

    35W updates

--- src/js/fed.js
+++ src/js/fed.js
@@ -82,7 +82,8 @@ $(document).ready(function() {
 
     $('.park-ride-map [data-toggle="popover"]').popover({
         html: true,
-        trigger: 'hover'
+        trigger: 'hover',
+        constraints: [ { to: 'scrollParent', pin: true } ]
         //placement: 'top'
     });
 
